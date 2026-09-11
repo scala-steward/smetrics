d938b2ecf0c277fe8301ccafef8dfd9ab87d2a28
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-09-11 16:44:03 +0200

    Update cats-helper to 3.13.1 (#460)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   val prometheusCommon = "io.prometheus" % "simpleclient_common" % prometheusVersion
   val scalatest = "org.scalatest" %% "scalatest" % "3.2.20"
   val weaver = "org.typelevel" %% "weaver-cats" % "0.13.0"
-  val `cats-helper` = "com.evolutiongaming" %% "cats-helper" % "3.12.2"
+  val `cats-helper` = "com.evolutiongaming" %% "cats-helper" % "3.13.1"
   val http4s = "org.http4s" %% "http4s-core" % "0.23.37"
   val doobie = "org.typelevel" %% "doobie-core" % "1.0.0-RC13"
 

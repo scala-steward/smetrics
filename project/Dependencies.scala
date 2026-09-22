4e3b27ee15f3696f21cd86703a2f683ab232ae6f
Scala Steward <scala_steward@virtuslab.com>
2026-09-22 16:14:56 +0000

    Update prometheus-metrics-core, ... to 1.9.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,7 +19,7 @@ object Dependencies {
   }
 
   object PrometheusV1 {
-    private val version = "1.8.0"
+    private val version = "1.9.0"
     val core = "io.prometheus" % "prometheus-metrics-core" % version
     val formats = "io.prometheus" % "prometheus-metrics-exposition-textformats" % version
   }

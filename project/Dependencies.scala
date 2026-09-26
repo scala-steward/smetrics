5aa2f06744c7c32486d6ea381f879338d8129da2
Scala Steward <scala_steward@virtuslab.com>
2026-09-26 16:31:50 +0000

    Update client4:cats, client4:core to 4.0.27

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -36,7 +36,7 @@ object Dependencies {
   }
 
   object Sttp4 {
-    private val version = "4.0.26"
+    private val version = "4.0.27"
     val core = "com.softwaremill.sttp.client4" %% "core" % version
     val catsBackend = "com.softwaremill.sttp.client4" %% "cats" % version
   }
